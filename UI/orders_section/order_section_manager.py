6ecff202233482000de6ad6cfe7a8b4ad9855f8d
emanuele-levels <emanuele@levelstech.it>
2025-02-07 09:52:33 +0100

    fixed orders_count behaviour

--- UI/orders_section/order_section_manager.py
+++ UI/orders_section/order_section_manager.py
@@ -430,6 +430,7 @@ class UpdateHandler:
         updated_df, _ = update_df(st.session_state.processed_data, self.new_values, self.name)
         st.session_state.processed_data = updated_df
         st.session_state.saved_updates.add(self.name)
+        self.orders_count += 1
         st.session_state[f'success_{self.name}'] = True
 
         auto_save_on_change()
@@ -533,7 +534,6 @@ class UpdateHandler:
 
     def show_success(self):
         st.success("Modifiche salvate con successo!")
-        self.orders_count += 1
         
         for n in st.session_state.numeri_pagamenti:
             if n not in st.session_state.pagamenti_da_aggiungere_lil.keys():
